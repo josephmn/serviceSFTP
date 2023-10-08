573f6ae42a991de5f85b72ac1dcd4e95e372dc8d
Joseph Magallanes <josephcarlos.jcmn@gmail.com>
2023-10-08 00:45:17 -0500

    aplicacion SFTP

---
+++ README.md
@@ -0,0 +1,128 @@
+![visual](https://img.shields.io/badge/Visual_Studio-2022-blue)
+![microsoft](https://img.shields.io/badge/Microsoft-yellowgreen)
+![windows10](https://img.shields.io/badge/windows_10-success)
+![windows11](https://img.shields.io/badge/windows_11-success)
+![windowsServer1](https://img.shields.io/badge/windows_server_2012-success)
+![windowsServer2](https://img.shields.io/badge/windows_server_2016-success)
+![windowsServer3](https://img.shields.io/badge/windows_server_2019-success)
+![OpenSSH](https://img.shields.io/badge/OpenSSH-important)
+![xml](https://img.shields.io/badge/XML-important)
+![net](https://img.shields.io/badge/.NET_Framework-v4.0-important)
+![ssh](https://img.shields.io/badge/SSH.NET-2020.0.2-important)
+
+# serviceSFTP (v1.0)
+
+Sincronizador para subir archivos a un SFTP, se tiene el servicio compilado en C#, se debe crear como servicio en Windows para que pueda ejecutarse en segundo plano.
+
+## Requisitos previos
+
+* Tener instalador Visual Studio 2022 en la que fue desarrollado el proyecto.
+
+## Instalación
+
+La ruta recomendada que se usa en el proyecto es la siguiente: **C:\SFTPservice**
+
+1.	Compilar el proyecto en C#, luego copiar los archivos que se han creado en la siguiente ruta:
+
+  	serviceSFTP\bin\Debug\
+  	
+    copiar todos los archivos y guardarlos en alguna unidad de preferencia que sea en la unidad:
+
+  	**C:\SFTPservice\app\”pegas_los_archivos”**
+
+2.	Cuando ya tengas los archivos copiados en la ruta recomendada, se debe guardar dentro el siguiente archivo root.xml (este es el archivo configurable):
+
+    ![Alt text](img/image.png)
+  	
+  	El archivo root.xml, lo adjuntare aquí como archivo para que lo puedan descargar.
+  	
+    Click aqui -> [root.xml](archivos/root.xml)
+
+  	También dejare el contenido que deben copiar si en caso no puedan descargar el archivo, solo deben guardar el contenido con el mismo nombre: root.xml en la misma ruta descrita.
+
+    ![img-estructura-xml-serviceSFTP](img/image-1.png)
+
+    Donde:
+  	
+    **host ->** ip o dominio del SFTP.  
+    **port ->** Puerto asignado a SFTP, por lo general es el 22.  
+    **username ->** usuario SFTP, de preferencia con privilegios altos.  
+    **password ->** contraseña del user SFTP.  
+    **directoryPath ->** Directorio en donde crearan los archivos a subir al SFTP.  
+    **filePaths ->** indicar el tipo de archivo a cargar.  
+    **NamedestinationDirectory ->** Nombre del directorio donde se moverán los archivos ya subidos al SFTP.  
+    **remoteFilePath ->** Directorio del SFTP donde se cargarán los archivos.  
+  	**privateKeyFilePath ->** Ruta del archivos donde se encuentra el privateKey (.ssh no .ppk).  
+    **logFilePath ->** Ruta en donde se guardaran los archivos log.  
+	**ambiente ->** Este puede ser QAS | DEV | TEST | PRD, recordar que el nombre se usara como ruta de directorio para almacenar los archivo, este se complementa con **directoryPath**. Ejemplo: C:\upload\QAS o C:\upload\PRD, dentro de esas carpetas se deben almacenar los archivo a cargar.  
+
+    Recordar:
+  	
+    Si tiene un archivo ppk -> putty, este no lo reconoce la libreria de SSH.NET, se necesita convertirlo, siga los siguientes pasos:
+    
+      I. Instale putty en su equipo.  
+     II. Luego cargue el archivo clave (.ppt) en puttygen.exe.  
+    III. Conversiones > Exportar clave OpenSSH (no la opción "forzar nuevo formato de archivo").  
+     IV. Esto creara un archivo que comienza con:  
+     ```
+    -----BEGIN RSA PRIVATE KEY-----  
+    MIIBOgIBAAJBAKj34GkxFhD90vcNLYLInFEX6Ppy1tPf9Cnzj4p4WGeKLs1Pt8Qu  
+    KUpRKfFLfRYC9AIKjbJTWit+CqvjWYzvQwECAwEAAQJAIJLixBy2qpFoS4DSmoEm  
+    o3qGy0t6z09AIJtH+5OeRV1be+N4cDYJKffGzDa88vQENZiRm0GRq6a+HPGQMd2k  
+    TQIhAKMSvzIBnni7ot/OSie2TmJLY4SwTQAevXysE2RbFDYdAiEBCUEaRQnMnbp7  
+    9mxDXDf6AU0cN/RPBjb9qSHDcWZHGzUCIG2Es59z8ugGrDY+pxLQnwfotadxd+Uy  
+    v/Ow5T0q5gIJAiEAyS4RaI9YG8EWx/2w0T67ZUVAw8eOMB6BIUg0Xcu+3okCIBOs  
+    /5OiPgoTdSy7bcF9IGpSE8ZgGKzgYQVZeN97YE00  
+    -----END RSA PRIVATE KEY-----
+    
+    Esto debe funcionar 👍.  
+3.	Luego de haber guardado los archivos en el paso uno, se debe crear el servicio con los siguientes comandos en Windows:
+
+    Abrir CMD.exe en modo administrador y ejecutar lo siguiente:
+
+    **Primer paso:**  
+    **sc.exe create SFTPservice binpath= C:\SFTPservice\app\serviceSFTP.exe start= auto**
+    
+    Donde:  
+    **SFTPservice ->** nombre que tendrá tu servicio, lo podrás ver en services.msc de Windows con el mismo nombre.  
+    **C:\SFTPservice\app\ ->** ruta creada en el punto 1, donde se copiaron los archivos de compilación del proyecto de C#.  
+    **start= auto ->** sentencia de comandos para indicar que este se inicie de manera automática al iniciar o reiniciar Windows.  
+    
+    Al ejecutar debería tener el siguiente resultado:
+  	
+    ![img-crear-service](img/image-2.png)
+
+    **Segundo paso:**  
+    Iniciar el servicio creado por CMD.exe, se debe ejecutar el siguiente comando:
+    
+    **net start SFTPservice**
+    
+    Donde:  
+    **SFTPservice ->** Es el nombre que se asignó al servicio en el paso 3.
+    
+    Al ejecutar debería tener el siguiente resultado:
+
+  	![img-iniciar-service](img/image-3.png)
+
+    **Tercer paso:**
+    Para detener el servicio por CMD.exe, se debe ejecutar el siguiente comando.
+    
+    **net stop SFTPservice**
+    
+    Donde:  
+    **SFTPservice ->** Es el nombre que se asignó al servicio en el paso 3.  
+    
+    De igual forma se puede ingresar a los servicios de Windows para iniciar o detenerlos de forma manual.
+
+4.	Para eliminar el servicio de Windows, ejecutar el siguiente comando en CMD modo administrador:
+
+    **sc.exe delete SFTPservice**
+    
+    Donde:  
+    **SFTPservice ->** nombre que tiene tu servicio, en caso sea otro el nombre cambiarlo.
+  	
+* Si todos los pasos enteriores se ejecutan de manera correcta, no debería haber problemas.
+
+## Uso
+
+Su uso puede servir como cron para poder subir archivos de manera masiva usando SFTP.
---
+++ serviceSFTP/Program.cs
@@ -0,0 +1,342 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Renci.SshNet;
+using Renci.SshNet.Messages;
+
+namespace serviceSFTP
+{
+    public class SftpConfig
+    {
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string directoryPath { get; set; }
+        public string filePaths { get; set; }
+        public string NamedestinationDirectory { get; set; }
+        public string remoteFilePath { get; set; }
+        public string privateKeyFilePath { get; set; }
+        public string logFilePath { get; set; }
+        public string ambiente { get; set; }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var isService = !(Debugger.IsAttached || args.Contains("--console"));
+
+            var builder = new HostBuilder()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<Worker>();
+                });
+
+            if (isService)
+            {
+                // Ejecutar la aplicación como un servicio de Windows
+                using (var host = builder.Build())
+                {
+                    ServiceBase.Run(new ServiceBase[] { new WindowsService(host) });
+                }
+            }
+            else
+            {
+                // Ejecutar la aplicación de consola para fines de desarrollo o prueba
+                using (var host = builder.Build())
+                {
+                    host.Run();
+                }
+            }
+        }
+    }
+
+    public class WindowsService : ServiceBase
+    {
+        private readonly IHost _host;
+
+        public WindowsService(IHost host)
+        {
+            _host = host;
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            _host.Start();
+        }
+
+        protected override void OnStop()
+        {
+            _host.StopAsync().GetAwaiter().GetResult();
+        }
+    }
+
+    public class Worker : IHostedService
+    {
+        private Timer _timer;
+        private readonly object lockObject = new object();
+        private bool isRunning = false;
+        private string txtlog;
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _timer = new Timer(DoWork, null, TimeSpan.Zero, TimeSpan.FromSeconds(60)); // Aquí puedes ajustar el intervalo de tiempo
+            return Task.CompletedTask;
+        }
+
+        private void InitializeLogFile(string logFilePath)
+        {
+            // Crear el archivo de registro si no existe
+            if (!File.Exists(logFilePath))
+            {
+                using (StreamWriter writer = File.CreateText(logFilePath))
+                {
+                    writer.WriteLine($"> Inicio del registro: {DateTime.Now}");
+                }
+            }
+            else
+            {
+                using (StreamWriter writer = File.AppendText(logFilePath))
+                {
+                    writer.WriteLine($"> Inicio del registro: {DateTime.Now}");
+                }
+            }
+        }
+
+        private void DoWork(object state)
+        {
+            string log_service_all = @"C:\SFTPservice\log\error_service_SFTP.txt";
+
+            if (!isRunning)
+            {
+                lock (lockObject)
+                {
+                    if (isRunning)
+                    {
+                        // Otra instancia ya está en progreso, no hagas nada
+                        return;
+                    }
+
+                    isRunning = true;
+                }
+
+                Console.WriteLine("Servicio en funcionamiento...");
+
+                try
+                {
+                    string rutaArchivoXml = @"C:\SFTPservice\root.xml"; // Reemplaza esto con la ruta real de tu archivo XML
+
+                    XDocument xdoc = XDocument.Load(rutaArchivoXml); // Cargamos el archivo XML usando XDocument
+
+                    // Creamos una instancia de la clase SftpConfig para almacenar los valores del XML
+                    SftpConfig config = new SftpConfig();
+
+                    // Accedemos a los elementos del XML y almacenamos los valores en las variables
+                    config.Host = xdoc.Root.Element("host")?.Value;
+                    int.TryParse(xdoc.Root.Element("port")?.Value, out int port);
+                    config.Port = port;
+                    config.Username = xdoc.Root.Element("username")?.Value;
+                    config.Password = xdoc.Root.Element("password")?.Value;
+                    config.directoryPath = xdoc.Root.Element("directoryPath")?.Value;
+                    config.filePaths = xdoc.Root.Element("filePaths")?.Value;
+                    config.NamedestinationDirectory = xdoc.Root.Element("NamedestinationDirectory")?.Value;
+                    config.remoteFilePath = xdoc.Root.Element("remoteFilePath")?.Value;
+                    config.privateKeyFilePath = xdoc.Root.Element("privateKeyFilePath")?.Value;
+                    config.logFilePath = xdoc.Root.Element("logFilePath")?.Value;
+                    config.ambiente = xdoc.Root.Element("ambiente")?.Value;
+
+                    DateTime currentDate = DateTime.Today;
+                    txtlog = Convert.ToString(currentDate.ToString("yyyy-MM-dd"));
+                    string archivolog = string.Concat(config.logFilePath, txtlog, ".txt");
+
+                    InitializeLogFile(archivolog);
+
+                    if (!string.IsNullOrEmpty(config.ambiente))
+                    {
+                        // Verificamos si se obtuvieron los valores correctamente
+                        if (config.Host != null && config.Port != 0 && config.Username != null && (config.Password == null || config.privateKeyFilePath != null))
+                        {
+                            // Información de conexión al servidor SFTP
+                            //string host = "1.0.0.0"; // host
+                            //int port = 22; // Puerto SFTP predeterminado
+                            //string username = "admin"; // usuario
+                            //string password = "clave"; // password
+
+                            // logica para saber de donde se subiran los archivos
+                            string new_directorio = Path.Combine(config.directoryPath, config.ambiente);
+
+                            //string directoryPath = "C:\\upload\\"; // Directorio que deseas cargar
+                            string[] filePaths = Directory.GetFiles(new_directorio, config.filePaths); // Cargar todos los archivos del directorio
+                            string destinationDirectory = Path.Combine(new_directorio, config.NamedestinationDirectory); // Nombre del directorio de destino despues de subir el archivo
+
+                            if (filePaths.Length == 0)
+                            {
+                                Console.WriteLine($"No hay archivos para procesar. -> {config.ambiente}");
+                                WriteToLog(archivolog, $"No hay archivos para procesar.  -> {config.ambiente}");
+                            }
+
+                            // Procesar los archivos cargados
+                            foreach (string filePath in filePaths)
+                            {
+                                // Console.WriteLine($"Archivo encontrado: {filePath}");
+                                // Aquí realiza la logica de subir los archivos a un servidor SFTP
+
+                                string fileName = Path.GetFileName(filePath);
+                                //string directorio_name = Path.GetDirectoryName(filePath);
+                                //string localFilePath = string.Concat(directorio_name, fileName); // Ruta local del archivo que deseas subir
+                                string remoteFilePath = string.Concat(config.remoteFilePath, fileName); // Ruta remota donde se almacenará el archivo en el servidor SFTP
+
+                                try
+                                {
+                                    if (string.IsNullOrEmpty(config.Password))
+                                    {
+                                        // instanciamos la clase
+                                        PrivateKeyFile privateKeyFile = new PrivateKeyFile(config.privateKeyFilePath);
+
+                                        var connectionInfo = new ConnectionInfo(config.Host, config.Port, config.Username, new PrivateKeyAuthenticationMethod(config.Username, new PrivateKeyFile[] { privateKeyFile }));
+
+                                        // Crear la conexión SFTP
+                                        using (var client = new SftpClient(connectionInfo))
+                                        {
+                                            client.Connect(); // Conectar al servidor
+                                            WriteToLog(archivolog, $"Conexión con privateKey. -> {config.ambiente}");
+
+                                            // Verificar si la conexión se estableció con éxito
+                                            if (client.IsConnected)
+                                            {
+                                                Console.WriteLine($"Conexión SFTP establecida. -> {config.ambiente}");
+
+                                                using (var fileStream = File.OpenRead(filePath)) // Abrir el archivo local para subirlo
+                                                {
+                                                    client.UploadFile(fileStream, remoteFilePath); // Subir el archivo al servidor SFTP
+                                                    Console.WriteLine($"Archivo subido con éxito: {filePath}"); // revision en consola del archivo que se esta cargando
+                                                    WriteToLog(archivolog, $"Archivo subido con éxito: {filePath}");
+                                                }
+
+                                                string destinationFilePath = Path.Combine(destinationDirectory, fileName);
+
+                                                try
+                                                {
+                                                    File.Move(filePath, destinationFilePath);
+                                                    Console.WriteLine($"Archivo {config.filePaths}: {fileName} movido a: {destinationFilePath}");
+                                                    WriteToLog(archivolog, $"Archivo {config.filePaths}: {fileName} movido a: {destinationFilePath}");
+                                                }
+                                                catch (IOException ex)
+                                                {
+                                                    WriteToLog(archivolog, $"Error al mover el archivo: {ex.Message}");
+                                                }
+                                            }
+                                            else
+                                            {
+                                                Console.WriteLine("No se pudo establecer la conexión SFTP."); // mensaje de error que no se puedo concectar al SFTP
+                                                WriteToLog(archivolog, "No se pudo establecer la conexión SFTP.");
+                                            }
+
+                                            client.Disconnect(); // Cerrar la conexión SFTP
+                                        }
+                                    }
+                                    else
+                                    {
+                                        // Crear la conexión SFTP
+                                        using (var client = new SftpClient(config.Host, config.Port, config.Username, config.Password))
+                                        {
+                                            client.Connect(); // Conectar al servidor
+                                            WriteToLog(archivolog, $"Conexión con Password. -> {config.ambiente}");
+
+                                            // Verificar si la conexión se estableció con éxito
+                                            if (client.IsConnected)
+                                            {
+                                                Console.WriteLine($"Conexión SFTP establecida. -> {config.ambiente}");
+
+                                                using (var fileStream = File.OpenRead(filePath)) // Abrir el archivo local para subirlo
+                                                {
+                                                    client.UploadFile(fileStream, remoteFilePath); // Subir el archivo al servidor SFTP
+                                                    Console.WriteLine($"Archivo subido con éxito: {filePath}"); // revision en consola del archivo que se esta cargando
+                                                    WriteToLog(archivolog, $"Archivo subido con éxito: {filePath}");
+                                                }
+
+                                                string destinationFilePath = Path.Combine(destinationDirectory, fileName);
+
+                                                try
+                                                {
+                                                    File.Move(filePath, destinationFilePath);
+                                                    Console.WriteLine($"Archivo  {config.filePaths} : {fileName} movido a: {destinationFilePath}");
+                                                    WriteToLog(archivolog, $"Archivo  {config.filePaths} : {fileName} movido a: {destinationFilePath}");
+                                                }
+                                                catch (IOException ex)
+                                                {
+                                                    WriteToLog(archivolog, $"Error al mover el archivo: {ex.Message}");
+                                                }
+
+                                            }
+                                            else
+                                            {
+                                                Console.WriteLine("No se pudo establecer la conexión SFTP."); // mensaje de error que no se puedo concectar al SFTP
+                                                WriteToLog(archivolog, "No se pudo establecer la conexión SFTP.");
+                                            }
+
+                                            client.Disconnect(); // Cerrar la conexión SFTP
+                                        }
+                                    }
+
+                                }
+                                catch (Exception ex)
+                                {
+                                    Console.WriteLine("Error: " + ex.Message);
+                                    WriteToLog(archivolog, $"Error: {ex.Message}");
+                                }
+
+                            }
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("Error: No se pudieron obtener todos los valores del archivo XML.");
+                            WriteToLog(archivolog, "Error: No se pudieron obtener todos los valores del archivo XML.");
+                        }
+                    }
+                    else
+                    {
+                        WriteToLog(log_service_all, "Error no se definio el ambiente: PRD | QAS | DEV, campo del XML <ambiente> no puede estar vacío");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error al leer el archivo XML: " + ex.Message);
+                    WriteToLog(log_service_all, $"Error al leer el archivo XML: " + ex.Message);
+                }
+                finally
+                {
+                    isRunning = false;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Otra instancia ya está en progreso.");
+                WriteToLog(log_service_all, "Otra instancia ya está en progreso.");
+            }
+        }
+
+        private void WriteToLog(string filePath, string message)
+        {
+            using (StreamWriter writer = File.AppendText(filePath))
+            {
+                writer.WriteLine($"{DateTime.Now}: {message}");
+            }
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _timer?.Change(Timeout.Infinite, 0);
+            return Task.CompletedTask;
+        }
+    }
+}
